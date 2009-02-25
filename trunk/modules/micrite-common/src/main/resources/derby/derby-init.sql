c012b1f39c92cc3ea4fb51c4ecc542d1e2ca0ccf
bitorb <bitorb@8c8702aa-fe89-11dd-88c7-29a3b14d5316>
2009-02-25 11:36:26 +0000

    修改derby初始化语句编码为utf-8

--- trunk/modules/micrite-common/src/main/resources/derby/derby-init.sql
+++ trunk/modules/micrite-common/src/main/resources/derby/derby-init.sql
@@ -1,9 +1,9 @@
 insert  into t_user(id,name,username,password,email, disabled) values
-					(1,'Ò»°ãÓÃ»§','user','e10adc3949ba59abbe56e057f20f883e','user@gaixie.org',0),
-					(2,'¹ÜÀíÔ±','admin','e10adc3949ba59abbe56e057f20f883e','admin@gaixie.org',0);
+     (1,'ä¸€èˆ¬ç”¨æˆ·','user','e10adc3949ba59abbe56e057f20f883e','user@gaixie.org',0),
+     (2,'ç®¡ç†å‘˜','admin','e10adc3949ba59abbe56e057f20f883e','admin@gaixie.org',0);
 insert  into t_role(id,name,description) values 
-					(1,'ROLE_USER','ROLE_USER'),
-					(2,'ROLE_ADMIN','ROLE_ADMIN');
+     (1,'ROLE_USER','ROLE_USER'),
+     (2,'ROLE_ADMIN','ROLE_ADMIN');
 insert  into t_resource(id,type,value) values (1,'URL','/**');
 insert  into t_role_resource(role_id,resource_id) values (1,1),(2,1);
 insert  into t_user_role(user_id,role_id) values (1,1),(1,2),(2,1);
