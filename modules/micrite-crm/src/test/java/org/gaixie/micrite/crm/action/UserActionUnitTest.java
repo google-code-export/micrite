13224cb3423c0f087e8be777fb5e43961a48c9c9
xingzhaoyong@gmail.com <xingzhaoyong@gmail.com@8c8702aa-fe89-11dd-88c7-29a3b14d5316>
2009-03-05 03:01:11 +0000

    修改了一下单元测试用例，删除了resources。

--- modules/micrite-crm/src/test/java/org/gaixie/micrite/crm/action/UserActionUnitTest.java
+++ modules/micrite-crm/src/test/java/org/gaixie/micrite/crm/action/UserActionUnitTest.java
@@ -2,31 +2,72 @@ package org.gaixie.micrite.crm.action;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.gaixie.micrite.beans.User;
+import org.gaixie.micrite.crm.service.IUserService;
+import org.gaixie.micrite.crm.service.impl.UserServiceImplMock;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath:applicationContext-crm-bean-test.xml"})
 public class UserActionUnitTest {
-
-    @Autowired
-    private UserAction userAction;
-
-    @Test
-    public void testManage() {
+	
+	@Test
+    public void testManageWhenSomeUsers() {
+		//	准备serviceMock
+    	List<User> usersMock = new ArrayList<User>();
+    	User userMock = new User();
+    	userMock.setName("userName");
+    	usersMock.add(userMock);
+    	IUserService userServiceMock = new UserServiceImplMock();
+    	((UserServiceImplMock)userServiceMock).setUsers(usersMock);
+    	
+    	UserAction userAction = new UserAction(userServiceMock);
+    	//	调用待测试方法
         String result = userAction.manage();
+        List<User> users = userAction.getUsers();
+        //	断言查询结果长度为1
+        assertEquals(1, users.size());
+        User user = users.get(0);
+        //	断言查询结果用户的名字
+        assertTrue("userName".equals(user.getName()));
+        //    断言是否正确返回结果
         assertTrue(ActionSupport.SUCCESS.equals(result));
     }
 
-    @Test
+	@Test
+    public void testManageWhenNoUsers() {
+		//	准备serviceMock
+    	IUserService userServiceMock = new UserServiceImplMock();
+    	((UserServiceImplMock)userServiceMock).setUsers(new ArrayList<User>());
+
+    	UserAction userAction = new UserAction(userServiceMock);
+    	//	调用待测试方法
+		String result = userAction.manage();
+        List<User> users = userAction.getUsers();
+        assertNull(users);
+        assertTrue(ActionSupport.SUCCESS.equals(result));
+    }
+
+	@Test
     public void testDisable() {
-        //    调用待测试方法
+		//	准备serviceMock
+    	List<User> usersMock = new ArrayList<User>();
+    	User userMock = new User();
+    	userMock.setId(1);
+    	userMock.setDisabled(false);
+    	usersMock.add(userMock);
+    	IUserService userServiceMock = new UserServiceImplMock();
+    	((UserServiceImplMock)userServiceMock).setUsers(usersMock);	
+		
+    	UserAction userAction = new UserAction(userServiceMock);
+    	userAction.setId(1);
+        //	调用待测试方法
         String result = userAction.disabled();
+        assertTrue(userMock.isDisabled());
         //    断言是否正确返回结果
         assertTrue(ActionSupport.SUCCESS.equals(result));
     }
