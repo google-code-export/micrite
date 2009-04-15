0511b74904b13dba350197bcc44ee8090117f2da
bitorb <bitorb@8c8702aa-fe89-11dd-88c7-29a3b14d5316>
2009-04-15 10:15:35 +0000

    删除从HibernateDaoSupport继承的抽象类及接口，改为各模块直接继承HibernateDaoSupport

--- modules/micrite-crm/src/main/java/org/gaixie/micrite/crm/dao/hibernate/CustomerDaoImpl.java
+++ modules/micrite-crm/src/main/java/org/gaixie/micrite/crm/dao/hibernate/CustomerDaoImpl.java
@@ -24,13 +24,106 @@
 
 package org.gaixie.micrite.crm.dao.hibernate;
 
+import java.util.List;
+import org.gaixie.micrite.beans.Customer;
+import org.gaixie.micrite.beans.CustomerSource;
 import org.gaixie.micrite.crm.dao.ICustomerDao;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
 /**
  * 客户管理持久化实现，基于hibernate
  * @see org.gaixie.micrite.crm.dao.ICustomerDao
- * @see org.gaixie.micrite.crm.dao.hibernate.AbstractDaoImpl
  */
-public class CustomerDaoImpl extends AbstractDaoImpl implements ICustomerDao {
+public class CustomerDaoImpl extends HibernateDaoSupport implements ICustomerDao {
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.gaixie.micrite.dao.ICustomerDao#save(org.gaixie.micrite.beans.Customer)
+	 */
+	public void saveCustomer(Customer customer) {
+		getHibernateTemplate().save(customer);
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.gaixie.micrite.dao.ICustomerDao#update(org.gaixie.micrite.beans.Customer)
+	 */
+	public void updateCustomer(Customer customer) {
+		getHibernateTemplate().update(customer);
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.gaixie.micrite.dao.ICustomerDao#delete(org.gaixie.micrite.beans.Customer)
+	 */
+	public void deleteCustomer(Customer customer) {
+		getHibernateTemplate().delete(customer);
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.gaixie.micrite.dao.ICustomerDao#getCustomer(int)
+	 */
+	public Customer getCustomer(int id) {
+		return (Customer) getHibernateTemplate().get(Customer.class, id);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.gaixie.micrite.dao.ICustomerDao#getCustomerSource(int)
+	 */
+	public CustomerSource getCustomerSource(int id) {
+		return (CustomerSource) getHibernateTemplate().get(CustomerSource.class, id);
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.gaixie.micrite.dao.ICustomerDao#findAllCustomerSource()
+	 */
+	public List findAllCustomerSource() {
+		return getHibernateTemplate().find(
+				"from " + CustomerSource.class.getSimpleName() + " e");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.gaixie.micrite.dao.ICustomerDao#getCustomerCount()
+	 */
+	public int getCustomerCount() {
+		String sql = "select count(*) from " + Customer.class.getSimpleName();
+		return ((Number) getHibernateTemplate()
+				.getSessionFactory().getCurrentSession().createQuery(sql).uniqueResult()).intValue();
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.gaixie.micrite.dao.ICustomerDao#findCustomerExact(java.lang.String, java.lang.Object)
+	 */
+	public List findCustomerExact(String column, Object value) {
+		return getHibernateTemplate().find(
+				"from " + Customer.class.getSimpleName() + " e where " + column
+						+ " = ?", value);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.gaixie.micrite.dao.ICustomerDao#findCustomerVague(java.lang.String, java.lang.String)
+	 */
+	public List findCustomerVague(String column, String value) {
+		return getHibernateTemplate().find(
+				"from " + Customer.class.getSimpleName() + " e where " + column
+						+ " like ?", "%"+value+"%");
+	}
 }
