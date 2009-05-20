56c2e255d9f826eee7ada3e54335a882ac4b02d9
bitorb <bitorb@8c8702aa-fe89-11dd-88c7-29a3b14d5316>
2009-05-20 08:45:15 +0000

    修改Search表头的国际化，初步确定的布局标准。

--- trunk/modules/micrite-crm/src/main/webapp/crm/locale/micrite-crm-lang-zh_CN.js
+++ trunk/modules/micrite-crm/src/main/webapp/crm/locale/micrite-crm-lang-zh_CN.js
@@ -1,7 +1,13 @@
 function customerListLocale(){
     if(micrite.crm.customerList.SearchPanel) {
         Ext.override(micrite.crm.customerList.SearchPanel, {
-            searchText:'查找',
+        	searchButton:'查询',	        	
+        	searchCondition1:'条件分组 1',
+        	searchCondition2:'条件分组 2',
+        	searchCondition3:'条件分组 3',  
+        	searchCellphone:'手机号',        	
+        	searchStartTime:'起始时间',
+        	searchEndTime:'截至时间',	        	
             newCustomerLink:'<a href="crm/customerDetail.jsp" id="Customer Detail" class="inner-link">增加新客户</a>'
         });
     }	
