491a6966d47b2c37b4aaddc0822fecb9aec3f30f
cobracoderyu@gmail.com <cobracoderyu@gmail.com@8c8702aa-fe89-11dd-88c7-29a3b14d5316>
2009-07-13 03:46:34 +0000

    修改共用grid的代码实现方式，以及修改了customer模块测试grid，导入了extjs3的库

--- trunk/modules/micrite-crm/src/main/webapp/crm/customerList.js
+++ trunk/modules/micrite-crm/src/main/webapp/crm/customerList.js
@@ -1,287 +1,257 @@
 <script type="text/javascript">
 Ext.namespace('micrite.crm.customerList');
 
-micrite.crm.customerList.SearchPanel = function() {
-    //  查询条件分组名称数组
-    this.conNames = [this.searchCondition1, this.searchCondition2,this.searchCondition3];
-    //  查询条件分组组件组数组
-    this.conCmpGroups = [
-        [   this.searchCellphone, {xtype:'textfield', name:'customer.telephone', width:120}],
-        [   this.searchStartTime, {xtype:'datetimefield', name:'time', width:135,format:'Y-m-d H:i'}],
-        [   this.searchStartTime, {xtype:'datetimefield', name:'startTime', width:135,format:'Y-m-d H:i'},
-            this.searchEndTime, {xtype:'datetimefield', name:'endTime', width:135,format:'Y-m-d H:i'},
-            'Gb',   {xtype:'checkbox', name:'gb', width:40,height:20},
-            'Gn',   {xtype:'checkbox', name:'gn', width:40,height:20},
-            'Gi',   {xtype:'checkbox', name:'gi', width:40,height:20},
-            'Gw',   {xtype:'checkbox', name:'gw', width:40,height:20}]
-    ];
-    //  超链菜单项数组
-    this.actionButtonMenuItems =  [{
-        text:this.customerSourceBarChart,
-        iconCls :'bar-chart-icon',
-        scope:this,
-        handler:function() {
-            Ext.Ajax.request({
-                scope:this,
-                url:'crm/getCustomerSourceBarChart.action?'+(new Date()).getTime(),
-                params:{'customer.telephone':this.curConFields[0].getRawValue()},
-                method:'post',
-                success:function(response){
-                    obj = Ext.util.JSON.decode(response.responseText);
-                    var filename = obj.filename;
-                    var win;
-                    if(!(win = Ext.getCmp('customerSourceBarChart'))){
-                        win = new Ext.Window({
-                                id    : 'customerSourceBarChart',
-                                title   : this.customerSourceBarChart,
-                                closable : true,
-                                width    : 630,
-                                height   : 520,
-                                plain    : true,
-                                maximizable: true,
-                                html:    '<img src = "'+'/' + document.location.href.split("/")[3] +
-                                                 '/DisplayChart?filename='+filename+'">',
-                                layout   : 'fit'
-                            });
-                        win.show();
-                        win.center();
-                    }
-                },
-                failure:function(){
-                    Ext.Msg.alert('info','FALSE');
-                }
-            });
-        }
-    },{
-        text:this.customerSourcePieChart,
-        iconCls :'pie-chart-icon',
-        scope:this,
-        handler:function() {
-            Ext.Ajax.request({
-                scope:this,
-                url:'crm/getCustomerSourcePieChart.action?'+(new Date()).getTime(),
-                params:{'customer.telephone':this.curConFields[0].getRawValue()},
-                method:'post',
-                success:function(response){
-                    obj = Ext.util.JSON.decode(response.responseText);
-                    var filename = obj.filename;
-                    var win;
-                    if(!(win = Ext.getCmp('customerSourcePieChart'))){
-                        win = new Ext.Window({
-                                id    : 'customerSourcePieChart',
-                                title   : this.customerSourcePieChart,
-                                closable : true,
-                                width    : 630,
-                                height   : 520,
-                                plain    : true,
-                                maximizable: true,
-                                html:    '<img src = "'+'/' + document.location.href.split("/")[3]+
-                                                                       '/DisplayChart?filename='+filename+'">',
-                                layout   : 'fit'
-                            });
-                        win.show();
-                        win.center();
-                    }
-                },
-                failure:function(){
-                    Ext.Msg.alert('info','FALSE');
-                }
-            });
-        }
-    },{
-        text:this.newCustomer,
-        iconCls :'add-icon',
-        scope:this,
-        handler:function() {
-            var win;
-            if(!(win = Ext.getCmp('addCusetomerWindow'))){
-                win = new Ext.Window({
-                    id: 'addCusetomerWindow',
-                    title    : this.newCustomer,
-                    closable : true,
-                    autoLoad : {url: 'crm/customerDetail.js?'+(new Date()).getTime(),scripts:true},
-                    width    : 500,
-                    height   : 360,
-                    maximizable : true,
-                    layout:'fit'
-                });
-            }
-            win.show();
-            win.center();
-        }
-    }];    
+micrite.crm.customerList.SearchPanel = Ext.extend(micrite.ComplexEditorGrid, {
+	
+	colModelId:'ID',
+	colModelName : 'Name',
+	colModelMobile : 'Mobile',
+	colModelSource : 'Source',
+	customerSourceChart:'Customer Source Chart',
+	
+	initComponent:function() {
+	//来源下拉框
+	var RecordDef = 
+	   Ext.data.Record.create([{name: 'id'},{name: 'name'}]);
+	       
+	var store = new Ext.data.Store({
+		id: Ext.id(),    
+		autoLoad:true,
+		//设定读取的地址
+		proxy : new Ext.data.HttpProxy({url: this.urlPrefix + '/crm/getCustomerPartner.action'}),    
+		//设定读取的格式    
+		reader : new Ext.data.JsonReader({    
+			id:"id"
+		}, RecordDef),
+		remoteSort: true   
+	});
+	// 下拉框renderer
+	var comboBoxRenderer = function(combo) {
+		return function(value) {
+			var idx = combo.store.find(combo.valueField, value);
+			var rec = combo.store.getAt(idx);
+			return (rec === null ? value : rec.get(combo.displayField) );
+	
+		};
+	};
+	var combo = new Ext.form.ComboBox({
+		valueField: "id",
+		displayField: "name",
+		store: store,
+		triggerAction :'all',
+		lazyRender:false 
+	});
+	var config = {
+	        compSet: [
+	             {url:0,reader:0,columns:0,bbarAction:0},
+	             {url:0,reader:0,columns:0,bbarAction:0},
+	             {url:0,reader:0,columns:1}
+	        ],
+			searchMenu : [
+				 this.searchCondition1,
+				 this.searchCondition2,
+				 this.searchCondition3
+			],
+			searchFields :[[
+	             this.searchCellphone,
+	             {xtype:'textfield',
+	              name:'customer.telephone',
+	              width:120}
+	            ],[
+	             this.searchStartTime,
+	             {xtype:'datefield',
+	              name:'mydate',
+	              width:135}
+	            ],[
+	             this.searchStartTime,
+	             {xtype:'datefield', name:'startTime', width:135},
+	             this.searchEndTime,
+	             {xtype:'datefield', name:'endTime', width:135},
+	             'Gb',
+	             {xtype:'checkbox', name:'gb', width:40,height:20},
+	             'Gn',
+	             {xtype:'checkbox', name:'gn', width:40,height:20},
+	             'Gi',
+	             {xtype:'checkbox', name:'gi', width:40,eight:20},
+	             'Gw',
+	             {xtype:'checkbox', name:'gw', width:40,height:20}
+	        ]],
+	        urls: ['/crm/findCustomer.action','/crm/findCustomerNew.action'],
+	        readers : [[
+			     {name: 'id',type:'int'},
+			     {name: 'name'},
+			     {name: 'telephone'},
+			     {name: 'customer_source_id'}
+	        ]],
+			columnsArray: [[
+		          {
+		          	header:this.colModelName,
+		          	width:100, sortable: true,dataIndex: 'name',
+		          	editor:new Ext.form.TextField({allowBlank: false})
+		          },
+		          {
+		          	header: this.colModelMobile,
+		          	width: 100, sortable: true, dataIndex: 'telephone',
+		          	editor:new Ext.form.TextField({allowBlank: false})
+		          },
+		          {
+		          	header: this.colModelSource,
+		          	width: 100, sortable: true, dataIndex: 'customer_source_id',
+		          	editor:combo,renderer:comboBoxRenderer(combo)
+		          },
+		          new Ext.grid.CheckboxSelectionModel()
+			 ],[
+                  {
+                    header:'col1',
+                    width:100, sortable: true,dataIndex: 'name',
+                    editor:new Ext.form.TextField({allowBlank: false})
+                  },
+                  {
+                    header: 'col2',
+                    width: 100, sortable: true, dataIndex: 'telephone',
+                    editor:new Ext.form.TextField({allowBlank: false})
+                  },
+                  {
+                    header: 'col3',
+                    width: 100, sortable: true, dataIndex: 'customer_source_id',
+                    editor:combo,renderer:comboBoxRenderer(combo)
+                  },
+                  new Ext.grid.CheckboxSelectionModel()
+             ]],
+	         tbarActions : [{
+	        	 text:this.customerSourceBarChart,
+	        	 iconCls :'bar-chart-icon',
+	        	 scope:this,
+	        	 handler:this.getBarChart
+	         },{
+	        	 text:this.customerSourcePieChart,
+	        	 iconCls :'pie-chart-icon',
+	        	 scope:this,
+	        	 handler:this.getPieChart
+	         },{
+	        	 text:this.newCustomer,
+	        	 iconCls :'add-icon',
+	        	 scope:this,
+	        	 handler:this.addCustomer
+	         }],
+	         bbarActions:[[{
+	        	 text:mbLocale.deleteButton, 
+	        	 iconCls :'delete-icon',
+	        	 scope:this, 
+	        	 handler:this.deleteCustomer
+	         },{
+	        	 text:mbLocale.submitButton, 
+	        	 iconCls :'save-icon',
+	        	 scope:this, 
+	        	 handler:this.saveCustomer
+	         }]]
+	    }; // eo config object
+
+		// apply config
+		Ext.apply(this, Ext.apply(this.initialConfig, config)); 
+		micrite.crm.customerList.SearchPanel.superclass.initComponent.apply(this, arguments);
+    }, // eo function initComponent
     
-    //  查询请求的url
-    this.searchRequestURL = ['/' + document.location.href.split("/")[3] + '/crm/findCustomer.action',null,null];
-    
-    //  查询结果数据按此格式读取
-    this.resultDataFields = [[
-        {name: 'id',type:'int'},
-        {name: 'name'},
-        {name: 'telephone'},
-        {name: 'customer_source_id'}
-    ]];
-    this.comboGrid = [{url:0,reader:0,column:0,button:0},{url:0,reader:0,column:0,button:0},{url:0,reader:0,column:0,button:0}];
-    //  查询结果行选择模型
-    this.resultRowSelectionModel = new Ext.grid.CheckboxSelectionModel();
-    
-    //来源下拉框
-    var RecordDef = Ext.data.Record.create([    
-            {name: 'id'},{name: 'name'}                   
-    ]); 
-    var store = new Ext.data.Store({
-        id: Ext.id(),    
-        autoLoad:true,
-        //设定读取的地址
-        proxy: new Ext.data.HttpProxy({url: '/' + document.location.href.split("/")[3] + '/crm/getCustomerPartner.action'}),    
-        //设定读取的格式    
-        reader: new Ext.data.JsonReader({    
-            id:"id"
-        }, RecordDef),
-        remoteSort: true   
-    });
-    //下拉框renderer    
-    comboBoxRenderer = function(combo) {
-        return function(value) {
-            var idx = combo.store.find(combo.valueField, value);
-            var rec = combo.store.getAt(idx);
-            return (rec === null ? value : rec.get(combo.displayField) );
-            
+	getBarChart : function(o,t,id) {
+		var c1 = {id:'customerList.barchar',
+		  title:this.customerSourceBarChart};
+		var c2 = {
+            url: this.urlPrefix + '/crm/getCustomerSourceBarChart.action',
+            params:{'customer.telephone':this.curFields[0].getRawValue()}
         };
-    };
-    var combo = new Ext.form.ComboBox({
-                valueField: "id",
-                displayField: "name",
-                store: store,
-                triggerAction :'all',
-                lazyRender:false 
-    });
-    
-    //  查询结果列
-    this.resultColumns = [[
-         {header: this.colModelName, width: 100, sortable: true, dataIndex: 'name',editor:new Ext.form.TextField({
-            allowBlank: false
-         })},
-         {header: this.colModelMobile, width: 100, sortable: true, dataIndex: 'telephone',editor:new Ext.form.TextField({
-            allowBlank: false
-         })},
-         {header: this.colModelSource, width: 100, sortable: true, dataIndex: 'customer_source_id',editor:combo,renderer:comboBoxRenderer(combo)},
-         this.resultRowSelectionModel
-    ]];
-     this.edit = true;
-    //  动作按钮数组
-    this.resultProcessButtons = [[{
-        text:mbLocale.deleteButton, 
-        iconCls :'delete-icon',
-        scope:this, 
-        handler:function() {
-            var customerIds = this.resultGrid.selModel.selections.keys;
-            var deleteRolesFun = function(buttonId, text, opt) {
-                if (buttonId == 'yes') {
-                    Ext.Ajax.request({
-                        url:'/' + document.location.href.split("/")[3] + '/crm/deleteCustomer.action',
-                        params:{'customerIds':customerIds},
-                        scope:this,
-                        callback:function(options,success,response) {
-                            if (Ext.util.JSON.decode(response.responseText).success) {
-                                this.refresh();
-                                obj = Ext.util.JSON.decode(response.responseText);
-                                showMsg('success', obj.message);
-                            }else{
-                                obj = Ext.util.JSON.decode(response.responseText);
-                                showMsg('failure', obj.message);                                
-                            }
-                        }
-                    });
-                }
-            };
-            Ext.Msg.show({
-                title:mbLocale.infoMsg,
-                msg: mbLocale.delConfirmMsg,
-                buttons: Ext.Msg.YESNO,
-                scope: this,
-                fn: deleteRolesFun,
-                icon: Ext.MessageBox.QUESTION
-            });        
-        }
-    },{
-        text:mbLocale.submitButton, 
-        iconCls :'save-icon',
-        scope:this, 
-        handler:function() {
-            var store = this.resultGrid.getStore();
-            if(store.getModifiedRecords().length!=1){
-                Ext.MessageBox.alert(mbLocale.infoMsg,mbLocale.gridRowEditMsg);
-                return;
-            }
-            var customer = store.getModifiedRecords()[0];
-            var updateRolesFun = function(buttonId, text, opt) {
-                if (buttonId == 'yes') {
-                    Ext.Ajax.request({
-                        url:'/' + document.location.href.split("/")[3] + '/crm/editCustomer.action',
-                        params:{'customerSourceId':customer.get('customer_source_id'),
-                                'customer.name':customer.get('name'),
-                                'customer.telephone':customer.get('telephone'),
-                                'customer.id':customer.get('id')},
-                        scope:this,
-                        callback:function(options,success,response) {
-                            if (Ext.util.JSON.decode(response.responseText).success) {
-                                store.commitChanges();
-                                obj = Ext.util.JSON.decode(response.responseText);
-                                showMsg('success', obj.message);
-                            }else{
-                                obj = Ext.util.JSON.decode(response.responseText);
-                                showMsg('failure', obj.message);                                
-                            }
-                        }
-                    });
-                }
-            };
-            Ext.Msg.show({
-                title:mbLocale.infoMsg,
-                msg: mbLocale.updateConfirmMsg,
-                buttons: Ext.Msg.YESNO,
-                scope: this,
-                fn: updateRolesFun,
-                icon: Ext.MessageBox.QUESTION
-            });        
-        }
-    }]];
-    
-    micrite.crm.customerList.SearchPanel.superclass.constructor.call(this);
-};
+		var win = this.genChartWindow(c1,c2);
+	}, //eof getBarChart
+	
+	getPieChart : function() {
+		var c1 = {title:this.customerSourcePieChart};
+        var c2 = {
+            url: this.urlPrefix + '/crm/getCustomerSourcePieChart.action',
+            params:{'customer.telephone':this.curFields[0].getRawValue()}
+        };
+        this.genChartWindow(c1,c2);
+	}, //eof getPieChart
+	
+	addCustomer : function() {
+			var win = micrite.util.genWindow({
+                title    : this.newCustomer,
+                autoLoad : {url: this.urlPrefix + '/crm/customerDetail.js',scripts:true},
+                width    : 500,
+                height   : 360,
+            });
+	}, //eof addCustomer
+	
+	deleteCustomer : function() {
+		var customerIds = this.selModel.selections.keys;
+		var deleteRolesFun = function(buttonId, text, opt) {
+			if (buttonId == 'yes') {
+				micrite.util.ajaxRequest({
+                    url: this.urlPrefix + '/crm/deleteCustomer.action',
+                    params:{'customerIds':customerIds},
+                    success:function(r,o){
+                        this.store.reload();
+                    },
+                    failure:Ext.emptyFn
+                },this);
+			}
+		};
+		Ext.Msg.show({
+			title:mbLocale.infoMsg,
+			msg: mbLocale.delConfirmMsg,
+			buttons: Ext.Msg.YESNO,
+			scope: this,
+			fn: deleteRolesFun,
+			icon: Ext.MessageBox.QUESTION
+		});        
+	}, //eof deleteCustomer
+	
+	saveCustomer : function() {
+		var store = this.getStore();
+		if(store.getModifiedRecords().length!=1){
+			Ext.MessageBox.alert(mbLocale.infoMsg,mbLocale.gridRowEditMsg);
+			return;
+		}
+		var customer = store.getModifiedRecords()[0];
+		var updateRolesFun = function(buttonId, text, opt) {
+			if (buttonId == 'yes') {
+				micrite.util.ajaxRequest({
+					url: this.urlPrefix + '/crm/editCustomer.action',
+					params:{'customerSourceId':customer.get('customer_source_id'),
+					'customer.name':customer.get('name'),
+					'customer.telephone':customer.get('telephone'),
+					'customer.id':customer.get('id')},
+					scope:this,
+					success:function(r,o){
+                        this.getStore().commitChanges();
+                    },
+                    failure:Ext.emptyFn
+				});
+			}
+		};
+		Ext.Msg.show({
+			title:mbLocale.infoMsg,
+			msg: mbLocale.updateConfirmMsg,
+			buttons: Ext.Msg.YESNO,
+			scope: this,
+			fn: updateRolesFun,
+			icon: Ext.MessageBox.QUESTION
+		});        
+	} // eof saveCustomer
+}); //eo extend
 
-Ext.extend(micrite.crm.customerList.SearchPanel, micrite.panel.ComplexSearchPanel, {
-    colModelId : 'ID',
-    colModelName : 'Name',
-    colModelMobile : 'Mobile',
-    colModelSource : 'Source',
-    customerSourceChart:'Customer Source Chart'
-});
-
-//  处理多语言
+// 处理多语言
 try {customerListLocale();} catch (e) {}
-/**
- * 由于收到的数据是个数组，所以需要使用该函数提取值
- * 
- * @param val
- * @return
- */
-function sourceType(val) {
-    return val.name;
-}
 
 Ext.onReady(function() {
-    Ext.QuickTips.init();
-    var formPanel = new micrite.crm.customerList.SearchPanel();
-    if (mainPanel) {
-        mainPanel.getActiveTab().add(formPanel);
-        mainPanel.getActiveTab().doLayout();
-    } else {
-        var vp = new Ext.Viewport({
-            layout:'fit',
-            items:[formPanel]
-        });
-    }
+	Ext.QuickTips.init();
+	var formPanel = new micrite.crm.customerList.SearchPanel();
+	if (mainPanel) {
+		mainPanel.getActiveTab().add(formPanel);
+		mainPanel.getActiveTab().doLayout();
+	} else {
+		var vp = new Ext.Viewport({
+			layout:'fit',
+			items:[formPanel]
+		});
+	}
 });
 </script>
