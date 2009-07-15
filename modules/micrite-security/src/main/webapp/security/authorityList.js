d95bc9882767eb3223467f453a4c1717ef4fba78
maven.yu <maven.yu@8c8702aa-fe89-11dd-88c7-29a3b14d5316>
2009-07-15 07:46:03 +0000

    基于新的grid模板修改authorityList和authorityDetail

--- modules/micrite-security/src/main/webapp/security/authorityList.js
+++ modules/micrite-security/src/main/webapp/security/authorityList.js
@@ -1,183 +1,144 @@
 <script type="text/javascript">
 Ext.namespace('micrite.security.authorityList');
 
-micrite.security.authorityList.SearchPanel = function() {
-    //  查询条件分组名称数组
-    this.conNames = [''];
-    //  查询条件分组组件组数组
-    this.conCmpGroups = [
-        [this.name, {xtype:'textfield', name:'authority.name', width:180}]
-    ];
-    //  超链菜单项数组
-    this.actionButtonMenuItems =  [{
-        text:this.addAuthority,
-        iconCls :'add-icon',
-        scope:this,
-        handler:function() {
-            var win;
-            if(!(win = Ext.getCmp('addAuthorityWindow'))){
-                win = new Ext.Window({
-                    id: 'addAuthorityWindow',
-                    title    : this.addAuthority,
-                    closable : true,
-                    autoLoad : {url: 'security/authorityDetail.js?'+(new Date()).getTime(),scripts:true},
-                    width    : 500,
-                    height   : 360,
-                    maximizable : true,
-                    layout:'fit'
-                });
-            }
-            win.show();
-            win.center();
-        }
-    }];    
-    //  查询请求的url
-    this.searchRequestURL = ['/' + document.location.href.split("/")[3] + '/security/findAuthoritiesVague.action'];
-    
-    //  查询结果格式读取器
-    this.resultDataFields = [[
-        {name: 'id'},
-        {name: 'name'},
-        {name: 'value'},
-        {name: 'type'}
-    ]];
-    this.comboGrid = [{url:0,reader:0,column:0,button:0}];
-    //  查询结果行选择模型
-    this.resultRowSelectionModel = new Ext.grid.CheckboxSelectionModel();
-
-    //  查询结果列
-    this.resultColumns = [[
-                             {header: this.name, width: 120, sortable: true, dataIndex: 'name'},
-                             {header: this.value, width: 200, sortable: true, dataIndex: 'value',
-                                 editor: new Ext.form.TextField({
-                                     allowBlank: false
-                                 })
-                             },
-                             //{header: this.roles, width: 110, sortable: true, dataIndex: 'rolesString'},
-                             {header: this.type, width: 50, sortable: true, dataIndex: 'type'},
-                             this.resultRowSelectionModel
-                         ]];
-    this.edit = true;
-    //  动作按钮数组
-    this.resultProcessButtons = [[{
-        text:this.bindRole, 
-        iconCls :'bind-icon',
-        scope:this,
-        handler:function() {
-            //  选择的数据记录主键，形如“2, 4, 6, 10”
-            var authorityIds = this.resultGrid.selModel.selections.keys;
-            if(authorityIds.length != 1){
-                Ext.MessageBox.alert(mbLocale.infoMsg,mbLocale.gridRowSelectMsg);
-                return;
-            }
-            var authority = this.resultGrid.selModel.getSelections();
-            var win;
-            if(!(win = Ext.getCmp('roleSelectWindow'))){
-                win = new Ext.Window({
-                    id: 'roleSelectWindow',
-                    title    : this.bindRole  + ' -- ' +  authority[0].get('name') ,
-                    closable : true,
-                    autoLoad : {url: 'security/roleSelect.jsp?authorityId=' + authorityIds[0] + '&' + (new Date()).getTime(), scripts:true},
-                    width    : 600,
-                    height   : 420,
-                    maximizable : true,
-                    layout:'fit'
-                });
-            }
-            win.show();
-            win.center();
-        }
-    },{
-        text:mbLocale.deleteButton, 
-        iconCls :'delete-icon',
-        scope:this, 
-        handler:function() {
-            var authorityIds = this.resultGrid.selModel.selections.keys;
-            if(authorityIds.length<1){
-                Ext.MessageBox.alert(mbLocale.infoMsg,mbLocale.gridMultRowSelectMsg);
-                return;
-            }
-            var deleteAuthorityFun = function(buttonId, text, opt) {
-                if (buttonId == 'yes') {
-                    Ext.Ajax.request({
-                        url:'/' + document.location.href.split("/")[3] + '/deleteAuthority.action',
-                        params:{'authIds':authorityIds},
-                        scope:this,
-                        callback:function(options,success,response) {
-                            if (Ext.util.JSON.decode(response.responseText).success) {
-                                this.refresh();
-                                obj = Ext.util.JSON.decode(response.responseText);
-                                showMsg('success', obj.message);
-                            }else{
-                                obj = Ext.util.JSON.decode(response.responseText);
-                                showMsg('failure', obj.message);                                
-                            }
-                        }
-                    });
-                }
-            };
-            Ext.Msg.show({
-                title:mbLocale.infoMsg,
-                msg: mbLocale.delConfirmMsg,
-                buttons: Ext.Msg.YESNO,
-                scope: this,
-                fn: deleteAuthorityFun,
-                icon: Ext.MessageBox.QUESTION
-            });        
-        }
-    },{
-        text:mbLocale.submitButton, 
-        iconCls :'save-icon',
-        scope:this, 
-        handler:function() {
-            var store = this.resultGrid.getStore();
-            var authority;
-            if(store.getModifiedRecords().length!=1){
-                Ext.MessageBox.alert(mbLocale.infoMsg,mbLocale.gridRowEditMsg);
-                return;
-            }
-            authority = store.getModifiedRecords()[0];
-            var updateAuthorityFun = function(buttonId, text, opt) {
-                if (buttonId == 'yes') {
-                    Ext.Ajax.request({
-                        url:'/' + document.location.href.split("/")[3] + '/updateAuthority.action',
-                        params:{'authority.id':authority.get('id'),
-                                'authority.value':authority.get('value')},
-                        scope:this,
-                        callback:function(options,success,response) {
-                            if (Ext.util.JSON.decode(response.responseText).success) {
-                                store.commitChanges();
-                                obj = Ext.util.JSON.decode(response.responseText);
-                                showMsg('success', obj.message);
-                            }else{
-                                obj = Ext.util.JSON.decode(response.responseText);
-                                showMsg('failure', obj.message);                                
-                            }
-                        }
-                    });
-                }
-            };
-            Ext.Msg.show({
-                title:mbLocale.infoMsg,
-                msg: mbLocale.updateConfirmMsg,
-                buttons: Ext.Msg.YESNO,
-                scope: this,
-                fn: updateAuthorityFun,
-                icon: Ext.MessageBox.QUESTION
-            });        
-        }
-    }]];
-    
-    micrite.security.authorityList.SearchPanel.superclass.constructor.call(this);
-};
-
-Ext.extend(micrite.security.authorityList.SearchPanel, micrite.panel.ComplexSearchPanel, {
+micrite.security.authorityList.SearchPanel = Ext.extend(micrite.ComplexEditorGrid, {
     byName:'By Name',
     name:'Name',
     addAuthority:'Add Authority',
     value:'Value',
     type:'Type',
-    bindRole:'Bind Role'
+    bindRole:'Bind Role',
+
+    initComponent:function() { 
+    var sm = new Ext.grid.CheckboxSelectionModel();
+    var config = {
+            compSet:[{url:0,reader:0,columns:0,bbarAction:0}],
+            searchFields :[[
+                this.name, {xtype:'textfield', name:'authority.name', width:180}
+            ]],
+            urls: ['/security/findAuthoritiesVague.action'],
+            readers : [[
+                {name: 'id'},
+                {name: 'name'},
+                {name: 'value'},
+                {name: 'type'}
+            ]],
+            columnsArray: [[
+                {header: this.name, width: 120, sortable: true, dataIndex: 'name'},
+                {header: this.value, width: 200, sortable: true, dataIndex: 'value',
+                    		editor: new Ext.form.TextField({allowBlank: false})},
+                {header: this.type, width: 50, sortable: true, dataIndex: 'type'},
+                sm
+             ]],
+             tbarActions : [{
+                 text:this.addAuthority,
+                 iconCls :'add-icon',
+                 scope:this,
+                 handler:this.addAuthorityFun
+             }],
+             bbarActions:[[{
+                 text:this.bindRole, 
+                 iconCls :'bind-icon',
+                 scope:this, 
+                 handler:this.bindRoleFun
+             },{
+                 text:mbLocale.deleteButton, 
+                 iconCls :'delete-icon',
+                 scope:this, 
+                 handler:this.deleteAuthorityFun
+             },{
+                 text:mbLocale.submitButton, 
+                 iconCls :'save-icon',
+                 scope:this, 
+                 handler:this.saveAuthorityFun
+             }]],
+             sm:sm
+        }; // eo config object
+    Ext.apply(this, Ext.apply(this.initialConfig, config)); 
+    micrite.security.authorityList.SearchPanel.superclass.initComponent.apply(this, arguments);    
+    },
+
+    addAuthorityFun :function() {
+        var win = micrite.util.genWindow({
+            id: 'addAuthorityWindow',
+            title    : this.addAuthority,
+            autoLoad : {url: this.urlPrefix + '/security/authorityDetail.js',scripts:true},
+            width    : 500,
+            height   : 360
+        });
+    },
+
+    bindRoleFun:function() {
+        //  选择的数据记录主键，形如“2, 4, 6, 10”
+        var authorityIds = this.selModel.selections.keys;
+        if(authorityIds.length!=1){
+            Ext.MessageBox.alert(mbLocale.infoMsg,mbLocale.gridRowSelectMsg);
+            return;
+        }
+        var authorities = this.selModel.getSelections();
+        var win = micrite.util.genWindow({
+            id: 'userSelectWindow',
+            title    : this.bindUser+' -- '+authorities[0].get('name'),
+            autoLoad : {url: this.urlPrefix + '/security/roleSelect.jsp?authorityId='+authorityIds[0],scripts:true},
+            width    : 500,
+            height   : 360
+        });
+    },
+
+    deleteAuthorityFun:function() {
+        var authorityIds = this.selModel.selections.keys;
+        var deleteAuthorities = function(buttonId, text, opt) {
+            if (buttonId == 'yes') {
+                micrite.util.ajaxRequest({
+                    url: this.urlPrefix + '/deleteAuthority.action',
+                    params:{'authIds':authorityIds},
+                    success:function(r,o){
+                        this.store.reload();
+                    },
+                    failure:Ext.emptyFn
+                },this);
+            }
+        };
+        Ext.Msg.show({
+            title:mbLocale.infoMsg,
+            msg: mbLocale.delConfirmMsg,
+            buttons: Ext.Msg.YESNO,
+            scope: this,
+            fn: deleteAuthorities,
+            icon: Ext.MessageBox.QUESTION
+        });        
+    },
+    
+    saveAuthorityFun:function() {
+        var store = this.getStore();
+        var authority;
+        if(store.getModifiedRecords().length!=1){
+            Ext.MessageBox.alert(mbLocale.infoMsg,mbLocale.gridRowEditMsg);
+            return;
+        }
+        authority = store.getModifiedRecords()[0];
+        var updateAuthorities = function(buttonId, text, opt) {
+            if (buttonId == 'yes') {
+                micrite.util.ajaxRequest({
+                    url: this.urlPrefix + '/updateAuthority.action',
+                    params:{'authority.id':authority.get('id'),
+                        'authority.value':authority.get('value')},
+                    success:function(r,o){
+                        this.store.commitChanges();
+                    },
+                    failure:Ext.emptyFn
+                },this);
+            }
+        };
+        Ext.Msg.show({
+            title:mbLocale.infoMsg,
+            msg: mbLocale.updateConfirmMsg,
+            buttons: Ext.Msg.YESNO,
+            scope: this,
+            fn: updateAuthorities,
+            icon: Ext.MessageBox.QUESTION
+        });        
+    }    
 });
 
 //  处理多语言
