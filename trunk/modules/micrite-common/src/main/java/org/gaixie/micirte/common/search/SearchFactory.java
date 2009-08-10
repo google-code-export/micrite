46ca4152c9db08121277b5a2d78d17cb5ac10373
maven.yu <maven.yu@8c8702aa-fe89-11dd-88c7-29a3b14d5316>
2009-08-10 10:10:15 +0000

    增加高级查询

--- trunk/modules/micrite-common/src/main/java/org/gaixie/micirte/common/search/SearchFactory.java
+++ trunk/modules/micrite-common/src/main/java/org/gaixie/micirte/common/search/SearchFactory.java
@@ -1,8 +1,11 @@
 package org.gaixie.micirte.common.search;
 
+import java.lang.reflect.Field;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
+
+import ognl.OgnlOps;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.time.DateUtils;
@@ -16,7 +19,7 @@ public class SearchFactory {
      * @throws ParseException 
      */
     public static void main(String[] args) throws ParseException {
-        String str = "[name,yubo,like,string],[telephone,13810770810,=,string],[createDate,2009-08-02 00:00,>=,date],[interface_,1;2;3,in,numeric]";
+        String str = "[name,yubo,like],[telephone,13810770810,=],[createDate,2009-08-02 00:00,>=],[interface_,1;2;3,in]";
         String[] detach = detach(str, '[', ']');
         for(String s: detach)
             System.out.println("detach =" + s);
@@ -28,7 +31,7 @@ public class SearchFactory {
 
     /**
      * @param scarchBunch 格式：
-     * [name,value,relation,type],[name,value,relation,type],[name,value,relation,type]
+     * [name,value,relation],[name,value,relation],[name,value,relation]
      * name:    字段名称
      * value:   字段值
      * relation:条件关系，包含：=, <, >, <=, >=, like, in
@@ -42,61 +45,98 @@ public class SearchFactory {
         SearchBean[] search = new SearchBean[team.length];
         for(int i = 0; i < team.length; i++){
             String[] element = StringUtils.split(team[i], ',');
-            if(element == null || element.length != 4)
+            if(element == null || element.length != 3)
                 throw new ParseException("Unable to parse the string: " + scarchBunch, -1);
-            Object value;
-            if(element[3].equals("date")){
-                if(element[2].equals("<") || element[2].equals("<=")){
-                    if(element[1].length() == 10)
-                        element[1] += " 23:59:59";
-                    else
-                        element[1] += ":59";
-                }
-                else{
-                    if(element[1].length() == 10)
-                        element[1] += " 00:00:00";
-                    else
-                        element[1] += ":00";
-                }
-                value = DateUtils.parseDate(element[1], new String[] { "yy-MM-dd hh:mm:ss" });
-            }
-            else{
-                if(element[2].equals("like")){
-                    value = "%" + element[1] + "%";
-                }
-                else if(element[2].equals("in")){
-                    value = StringUtils.split(element[1], ';');
-                }
-                else
-                    value = element[1];
-            }
-            search[i] = new SearchBean(element[0], value, element[2], element[3]);
+            search[i] = new SearchBean(element[0], element[1], element[2]);
         }
         return search;
     }
     
     @SuppressWarnings("unchecked")
-    public static DetachedCriteria generateCriteria(Class entity, SearchBean[] searchBean){
+    public static DetachedCriteria generateCriteria(Class entity, SearchBean[] searchBean) throws NoSuchFieldException, ParseException{
         DetachedCriteria criteria = DetachedCriteria.forClass(entity);
         if(searchBean == null || searchBean.length == 0)
             return criteria;
+        Object value = null;
         for(int i = 0; i < searchBean.length; i++){
-            if(searchBean[i].equals("="))
-                criteria.add(Expression.eq(searchBean[i].getName(), searchBean[i].getValue()));
-            else if(searchBean[i].equals("<"))
-                criteria.add(Expression.lt(searchBean[i].getName(), searchBean[i].getValue()));
-            else if(searchBean[i].equals("<="))
-                criteria.add(Expression.le(searchBean[i].getName(), searchBean[i].getValue()));
-            else if(searchBean[i].equals(">"))
-                criteria.add(Expression.gt(searchBean[i].getName(), searchBean[i].getValue()));
-            else if(searchBean[i].equals("<="))
-                criteria.add(Expression.ge(searchBean[i].getName(), searchBean[i].getValue()));
-            else if(searchBean[i].equals("like"))
-                criteria.add(Expression.like(searchBean[i].getName(), searchBean[i].getValue()));
-            else if(searchBean[i].equals("in"))
-                criteria.add(Expression.in(searchBean[i].getName(), (String[])searchBean[i].getValue()));
+            Field field = entity.getDeclaredField(searchBean[i].getName());
+            Class type = field.getType();
+            value = convertValue(type, searchBean[i].getValue(), searchBean[i].getRelation());
+            if(searchBean[i].getRelation().equals("="))
+                criteria.add(Expression.eq(searchBean[i].getName(), value));
+            else if(searchBean[i].getRelation().equals("<"))
+                criteria.add(Expression.lt(searchBean[i].getName(), value));
+            else if(searchBean[i].getRelation().equals("<="))
+                criteria.add(Expression.le(searchBean[i].getName(), value));
+            else if(searchBean[i].getRelation().equals(">"))
+                criteria.add(Expression.gt(searchBean[i].getName(), value));
+            else if(searchBean[i].getRelation().equals(">="))
+                criteria.add(Expression.ge(searchBean[i].getName(), value));
+            else if(searchBean[i].getRelation().equals("like"))
+                criteria.add(Expression.like(searchBean[i].getName(), value));
+            else if(searchBean[i].getRelation().equals("in"))
+                criteria.add(Expression.in(searchBean[i].getName(), (List)value));
+            else if(searchBean[i].getRelation().equals("between"))
+                criteria.add(Expression.between(searchBean[i].getName(),
+                        ((List)value).get(0), ((List)value).get(1)));
         }
         return criteria;
+    }
+    
+    @SuppressWarnings("unchecked")
+    private static Object convertValue(Class type, String value, String relation) throws ParseException{
+        Object object;
+        if(relation.equals("between")){
+            List list = new ArrayList();
+            String[] betValue = StringUtils.split(value, ';');
+            if(type == java.util.Date.class){
+                if(betValue[0].length() == 10)
+                    betValue[0] += " 00:00:00";
+                else
+                    betValue[0] += ":00";
+                if(betValue[1].length() == 10)
+                    betValue[1] += " 23:59:59";
+                else
+                    betValue[1] += ":59";
+                list.add(DateUtils.parseDate(betValue[0], new String[] { "yyyy-MM-dd hh:mm:ss" }));
+                list.add(DateUtils.parseDate(betValue[1], new String[] { "yyyy-MM-dd hh:mm:ss" }));
+            }
+            else{
+                list.add(OgnlOps.convertValue(betValue[0], type));
+                list.add(OgnlOps.convertValue(betValue[1], type));
+            }
+            object = list;
+        }
+        else if(relation.equals("in")){
+            List list = new ArrayList();
+            for(String s : StringUtils.split(value, ';')){
+                list.add(OgnlOps.convertValue(s, type));
+            }
+            object = list;
+        }
+        else if(relation.equals("like")){
+            object = OgnlOps.convertValue("%" + value + "%", type);
+        }
+        else{
+            if(type == java.util.Date.class){
+                if(relation.equals("<") || relation.equals("<=")){
+                    if(value.length() == 10)
+                        value += " 23:59:59";
+                    else
+                        value += ":59";
+                }
+                else{
+                    if(value.length() == 10)
+                        value += " 00:00:00";
+                    else
+                        value += ":00";
+                }
+                object = DateUtils.parseDate(value, new String[] { "yyyy-MM-dd hh:mm:ss" });
+            }
+            else
+                object = OgnlOps.convertValue(value, type);
+        }
+        return object;
     }
     
     private static String[] detach(String str, char left, char right) throws ParseException{
